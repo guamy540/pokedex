07326d449dc81010afcac3cf87aae9ff79fa7735
guamy540 <49961360+guamy540@users.noreply.github.com>
2022-12-03 17:39:15 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+
+let pokemonFound = []
+let allPokemon = []
+
+fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+  .then(res => res.json())
+  .then(result => {
+    result.results.forEach(x => {
+      allPokemon.push(x.name)
+    })
+    localStorage.setItem('allPokemon', allPokemon)    
+  })
+  .catch(error => console.error('error', error))
+
+function findPokemonByName(input){
+  allPokemon.forEach(pokemon => {
+    if (pokemon.includes(input)) console.log(pokemon)
+  })
+}
+
+function getUserInput(){
+  let i = document.querySelector('#nameInput').value
+  return i;
+}
+
+function click(){
+  let input = getUserInput()
+  findPokemonByName(input)
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center p-4">
+
+      <div className='w-full text-center'>
+        <h1 className='text-4xl'>Title</h1>
+      </div>
+
+      <div className='flex flex-col justify-center align-center p-4 bg-red-500 mt-12 rounded-xl w-80'>
+        <div id='nameForm' className='flex flex-col justify-center align-center bg-red-500' onSubmit="return false;">
+          <label className='bg-red-500 text-white'>Search for a pokemon:</label>
+          <div className='flex justify-between'>
+          <input id='nameInput' type='text' 
+          className='bg-white input[type=text] rounded-md border-gray-300 shadow-sm pl-3 py-2
+          focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'>
+          </input>
+          <button onClick={() => {click()}}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} className='text-white text-3xl'></FontAwesomeIcon>
+          </button>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
