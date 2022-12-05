4ff2f3b4eb3debeb2a596e0ffe0e5fea7d20a84d
guamy540 <49961360+guamy540@users.noreply.github.com>
2022-12-05 10:47:38 -0800

    implemented working search/display by name

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import Results from './components/results';
+import Foundvalue from './components/Foundvalue/foundvalue';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
-
-let pokemonFound = []
-let allPokemon = []
-
-fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
-  .then(res => res.json())
-  .then(result => {
-    result.results.forEach(x => {
-      allPokemon.push(x.name)
-    })
-    localStorage.setItem('allPokemon', allPokemon)    
-  })
-  .catch(error => console.error('error', error))
-
-function findPokemonByName(input){
-  allPokemon.forEach(pokemon => {
-    if (pokemon.includes(input)) console.log(pokemon)
-  })
-}
-
-function getUserInput(){
-  let i = document.querySelector('#nameInput').value
-  return i;
-}
-
-function click(){
-  let input = getUserInput()
-  findPokemonByName(input)
-}
-
+import axios from 'axios';
 
 function App() {
+  let types = ['bug', 'dark', 'dragon', 'electric', 'fairy', 'fighting', 'fire', 
+  'flying', 'ghost', 'grass', 'ground', 'ice', 'normal', 'poison', 'psychic', 'rock', 'steel', 'water'] //all the types of pokemon
+
+  const [getUrl, setGetUrl] = useState("") //url that will be used for get request
+  const [pokemonFound, setPokemonFound] = useState(['charmander', 'squirtle', 'bulbasaur']) //starting pokemon
+  const [searchInput, setSearchInput] = useState("") //input from the search box
+  const [numberFound, setNumberFound] = useState(0) //number of pokemon displayed
+  const [displayFoundvalue, setDisplayFoundvalue] = useState(true) 
+  const [filterType, setFilterType] = useState("")
+
+  useEffect(()=>{
+    setNumberFound(pokemonFound.length)
+  },[pokemonFound]) //when pokemon found changes, change the number of results
+
+  useEffect(() => {
+    axios.get(getUrl).then(res => {
+      setPokemonFound(res.data.results.map(p => p.name))
+      if (searchInput){
+        setPokemonFound(prevPokemon => prevPokemon.filter(p => p.includes(searchInput)))
+      }
+    }) 
+  }, [getUrl, searchInput]) //if input or get request changes, show results
+
+  function clickTest(){
+    setGetUrl("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+    setSearchInput(document.querySelector('#nameInput').value)
+  }  //when the user clicks magnifying glass, change URL/search input 
+
   return (
     <div className="flex flex-col items-center p-4">
 
@@ -40,6 +42,7 @@ function App() {
         <h1 className='text-4xl'>Title</h1>
       </div>
 
+      
       <div className='flex flex-col justify-center align-center p-4 bg-red-500 mt-12 rounded-xl w-80'>
         <div id='nameForm' className='flex flex-col justify-center align-center bg-red-500' onSubmit="return false;">
           <label className='bg-red-500 text-white'>Search for a pokemon:</label>
@@ -48,12 +51,23 @@ function App() {
           className='bg-white input[type=text] rounded-md border-gray-300 shadow-sm pl-3 py-2
           focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'>
           </input>
-          <button onClick={() => {click()}}>
+          <button onClick={clickTest}>
             <FontAwesomeIcon icon={faMagnifyingGlass} className='text-white text-3xl'></FontAwesomeIcon>
           </button>
           </div>
         </div>
       </div>
+
+      <div id='filters' className='flex flex-wrap flex-row m-4 justify-center'>
+        {types.map((type) => {
+          return <button key={type} className='bg-neutral-900 w-24 m-4 text-center p-4 rounded-xl'><p className='text-white'>{type}</p></button>
+        })}
+      </div>
+
+      {displayFoundvalue? <Foundvalue number={numberFound}/> : null}
+
+      
+      <Results pokemon={pokemonFound}/>
 
     </div>
   );
---
+++ src/components/Foundvalue/foundvalue.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './foundvalue.css'
+
+export default function Foundvalue({number}) {
+  return (
+    <div>
+      <h3>{number} Pokemon found.</h3>
+    </div>
+  )
+}
